29f3abf371fdf09435c7b1bf88212e82aefea0d1
Steve Caldwell <scaldwell@gmail.com>
2015-01-02 12:03:22 -0800

    checkpoint on documentation

--- lib/MooseX/Storage/IO/AmazonDynamoDB.pm
+++ lib/MooseX/Storage/IO/AmazonDynamoDB.pm
@@ -1,4 +1,5 @@
 package MooseX::Storage::IO::AmazonDynamoDB;
+# ABSTRACT: Save Moose objects to AWS's DynamoDB, via MooseX::Storage.
 
 use strict;
 use 5.014;
@@ -220,17 +221,171 @@ __END__
 
 =encoding utf-8
 
-=head1 NAME
-
-MooseX::Storage::IO::AmazonDynamoDB - Blah blah blah
-
 =head1 SYNOPSIS
 
-  use MooseX::Storage::IO::AmazonDynamoDB;
+First, configure your Moose class via a call to Storage:
+
+  package MyDoc;
+  use Moose;
+  use MooseX::Storage;
+
+  with Storage(io => [ 'AmazonDynamoDB' => {
+      table_name => 'my_docs',
+      key_attr   => 'doc_id',
+  }]);
+
+  has 'doc_id'  => (is => 'ro', isa => 'Str', required => 1);
+  has 'title'   => (is => 'rw', isa => 'Str');
+  has 'body'    => (is => 'rw', isa => 'Str');
+  has 'tags'    => (is => 'rw', isa => 'ArrayRef');
+  has 'authors' => (is => 'rw', isa => 'HashRef');
+
+  1;
+
+Then create your table in DynamoDB.  You could also do this directly on AWS.
+
+  MyDoc->dynamo_db_create_table();
+
+Now you can store/load your class to DyanmoDB:
+
+  use MyDoc;
+
+  # Create a new instance of MyDoc
+  my $doc = MyDoc->new(
+      doc_id   => 'foo12',
+      title    => 'Foo',
+      body     => 'blah blah',
+      tags     => [qw(horse yellow angry)],
+      authors  => {
+          jdoe => {
+              name  => 'John Doe',
+              email => 'jdoe@gmail.com',
+              roles => [qw(author reader)],
+          },
+          bsmith => {
+              name  => 'Bob Smith',
+              email => 'bsmith@yahoo.com',
+              roles => [qw(editor reader)],
+          },
+      },
+  );
+
+  # Save it to DynamoDB
+  $doc->store();
+
+  # Load the saved data into a new instance
+  my $doc2 = MyDoc->load('foo12');
+
+  # This should say 'Bob Smith'
+  print $doc2->authors->{bsmith}{name};
 
 =head1 DESCRIPTION
 
-MooseX::Storage::IO::AmazonDynamoDB is
+MooseX::Storage::IO::AmazonDynamoDB is a Moose role that provides an io layer for L<MooseX::Storage> to store/load your Moose objects to Amazon's DynamoDB NoSQL database service.
+
+You should understand the basics of both L<MooseX::Storage> and L<DynamoDB|http://aws.amazon.com/dynamodb/> before using this module.
+
+This module uses L<Amazon::DynamoDB> as its client library to the DynamoDB service.
+
+By default it grabs authentication credentials using the same procedure as the AWS CLI, see L<AWS::CLI::Config>.  You can customize this behavior - see L<"CLIENT CONFIGURATION"> below.
+
+At a bare minimum the consuming class needs to tell this role what table to use and what field to use as a primary key - see L<table_name> and L<key_attr> below.
+
+=head1 PARAMETERS
+
+There are many parameters you can set when consuming this role that configure it in different ways.
+
+=head2 key_attr
+
+"key_attr" is a required parameter when consuming this role.  It specifies an attribute in your class that will provide the primary key value for storing your object to DynamoDB.  Currently only single primary keys are supported, or what DynamoDB calls "Hash Type Primary Key" (see their L<documentation|http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DataModel.html#DataModel.PrimaryKey>).  See the L<"SYNOPSIS"> for an example.
+
+=head2 table_name
+
+Specifies the name of the DynamoDB table to use for your objects - see the example in the L<"SYNOPSIS>.  Alternatively, you can return the table name via a class method - see L<"dynamo_db_table_name">.
+
+=head2 client_class
+=head2 host
+=head2 port
+=head2 ssl
+
+See L<"CLIENT CONFIGURATION">.
+
+=head2 table_name_method
+
+If you want to rename the L<"dynamo_db_table_name"> method.
+
+=head2 create_table_method
+
+If you want to rename the L<"dynamo_db_create_table"> method.
+
+=head2 client_attr
+
+If you want to rename the L<"dynamo_db_client"> attribute.
+
+=head2 client_class_method
+
+If you want to rename the L<"dynamo_db_client_class"> method.
+
+=head2 client_args_method
+
+If you want to rename the L<"dynamo_db_client_args"> method.
+
+=head1 ATTRIBUTES
+
+=head2 dynamo_db_client
+
+This role adds an attribute named "dynamo_db_client" to your consuming class.  This attribute holds an instance of Amazon::DynamoDB that will be used to communicate with the DynamoDB service.  See L<"CLIENT CONFIGURATION"> for more details.
+
+Note that you can change the name of this attribute when consuming this role via the 'client_attr' parameter.  For example, if you wrote:
+
+  with Storage(io => [ 'AmazonDynamoDB' => {
+      table_name  => 'my_docs',
+      key_attr    => 'doc_id',
+      client_attr => 'ddclient',
+  }]);
+
+Your object would now use the "ddclient" attribute to hold the DynamoDB client.
+
+=head1 METHODS
+
+These are methods that will be added to your consuming class.
+
+=head2 store
+
+=head2 load
+
+=head2 dynamo_db_create_table
+
+=head2 dynamo_db_client_class
+
+=head2 dynamo_db_client_args
+
+=head1 HOOKS
+
+These are methods that your consuming class can provide.
+
+=head2 dynamo_db_table_name
+
+A class method that will return the table name to use.  This method will be called if the L<"table_name"> parameter is not set.  So you could rewrite the Moose class in the L<"SYNOPSIS"> like this:
+
+  package MyDoc;
+  use Moose;
+  use MooseX::Storage;
+
+  with Storage(io => [ 'AmazonDynamoDB' => {
+      key_attr   => 'doc_id',
+  }]);
+
+  ...
+
+  sub dynamo_db_table_name {
+      my $class = shift;
+      return $ENV{DEVELOPMENT} ? 'my_docs_dev' : 'my_docs';
+  }
+
+You can also change the actual method name via the L<"table_name_method"> parameter.
+
+=head1 CLIENT CONFIGURATION
 
 =head1 AUTHOR
 
@@ -238,7 +393,7 @@ Steve Caldwell E<lt>scaldwell@gmail.comE<gt>
 
 =head1 COPYRIGHT
 
-Copyright 2014- Steve Caldwell
+Copyright 2015- Steve Caldwell
 
 =head1 LICENSE
 
@@ -247,4 +402,9 @@ it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
+=for :list
+* L<MooseX::Storage>
+* L<Amazon's DynamoDB Homepage|http://aws.amazon.com/dynamodb/>
+* L<Amazon::DynamoDB> - Perl DynamoDB client.
+* L<AWS::CLI::Config> - how configuration is done by default.
 =cut
